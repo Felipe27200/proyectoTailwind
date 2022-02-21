174a5c297e440aace44f3ddee99a6cb279266fa5
Felipe <felipe3841@gmail.com>
2022-02-21 09:21:08 -0500

    Flujo de columnas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,18 +10,47 @@
 </head>
 <body>
     <div class="container">
-        {{-- con grid rows, se le indica el número de filas que tendrá
-            en este caso se definen 2 --}}
-        <div class="grid grid-rows-2 grid-cols-4 gap-4">
-            <div class="bg-blue-200">A</div>
-            <div class="bg-blue-300">B</div>
-            {{-- Así se le indica que esta columna ocupará 2 filas, si no carga, puede ser por la cache del sitio --}}
-            <div class="bg-blue-400 col-span-2 row-span-2">C</div>
-            <div class="bg-blue-500">D</div>
-            <div class="bg-blue-700">E</div>
+        {{-- Gracias a grid flow col organiza de forma automática las columnas en una fila --}}
+        <div class="grid grid-flow-col">
+            <div class="bg-blue-100">1</div>
+            <div class="bg-blue-200">2</div>
+            <div class="bg-blue-300">3</div>
+            <div class="bg-blue-400">4</div>
+            <div class="bg-blue-500">5</div>
+            <div class="bg-blue-600">6</div>
+            <div class="bg-blue-700">7</div>
+            <div class="bg-blue-800">8</div>
+            <div class="bg-blue-900">9</div>
         </div>
 
+        {{-- Si se le añade un número de filas, organiza las columnas de forma
+            vertical en ellas, en este caso la primera columna iría de 1 a 3, la otra
+            de 4 a 6 y la última de 6 a 9, por lo que tienen 3 filas * 3 columnas --}}
+        <div class="grid grid-flow-col grid-rows-3 mt-6">
+            <div class="bg-blue-100">1</div>
+            <div class="bg-blue-200">2</div>
+            <div class="bg-blue-300">3</div>
+            <div class="bg-blue-400">4</div>
+            <div class="bg-blue-500">5</div>
+            <div class="bg-blue-600">6</div>
+            <div class="bg-blue-700">7</div>
+            <div class="bg-blue-800">8</div>
+            <div class="bg-blue-900">9</div>
+        </div>
 
+        {{-- Si se le define un número de columnas, la última columa queda vacía
+            ya que el número de div no es suficiente para completarla --}}
+        <div class="grid grid-flow-col grid-rows-3 grid-cols-4 mt-6">
+            <div class="bg-blue-100">1</div>
+            <div class="bg-blue-200">2</div>
+            <div class="bg-blue-300">3</div>
+            <div class="bg-blue-400">4</div>
+            <div class="bg-blue-500">5</div>
+            <div class="bg-blue-600">6</div>
+            <div class="bg-blue-700">7</div>
+            <div class="bg-blue-800">8</div>
+            <div class="bg-blue-900">9</div>
+        </div>
     </div>
 </body>
 </html>
